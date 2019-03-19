6166b76c53cba00b713212abd11b6dc0d4d5e81b
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-03-19 10:36:52 -0500

    Update Battleship.js

--- BattleShip Files/Battleship.js
+++ BattleShip Files/Battleship.js
@@ -23,5 +23,32 @@ var model = {
         { locations: ["10","20","30"], hits:["","",""]},
         { locations: ["32","33","34"], hits:["","",""]},
         { locations: ["63","64","65"], hits:["","",""]}
-    ]
+    ],
+    fire: function(guess){
+        for (var i = 0; i < this.numShips; i++){
+            var ship = this.ships[i];
+            var locations = ship.locations.indexOf(guess);
+            if(index>= 0){
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)){
+                    view.displayMessage("You sank my ship!")
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship){
+        for(var i = 0; i < this.shipLength; i++){
+            if(ship.hits[i] !== "hit"){
+            return false;
+            }
+        }
+        return true;
+    }
 };
